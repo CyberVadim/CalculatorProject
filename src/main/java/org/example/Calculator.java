42ca83b781fd2e86cc1b8d3c906889b5f9424c15
”Vadim <protasoff61@gmail.com>
2024-12-06 11:32:09 +0400

    миниправка

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,19 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-
         // Тут даны примеры для проверки того, что нужно согласно ТЗ
-        String example1 = "1 + 2";
+        String example1 = "1 + 20";
         String example2 = "1 / 2";
         String example3 = "1";
         String example4 = "1 * 2 1";
         String example5 = "ыыы";
-        String example6 = " ";
+        String example6 = "1 + a";
         String example7 = "1 + 2.1";
 
         try {
-            System.out.println(calc(example7)); // меняйте на любой example
+            System.out.println(calc(example1)); // меняйте на любой example
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -22,16 +23,13 @@ public class Calculator {
     public static String calc(String input) throws Exception {
         String[] parts = validateInput(input);
 
-        // Преобразуем в целые числа
+        // Преобразуем в целые числа и символ
         int a = parseInteger(parts[0]);
         int b = parseInteger(parts[2]);
+        String symbol = parts[1];
 
         // Проверка, что числа находятся в пределах от 1 до 10
         validateDiapason(a, b);
-
-        // Проверяем валидность символа арифметической операции
-        String symbol = parts[1];
-        validateOperationSymbol(symbol);
 
         // Арифметическая операция
         int result = arithmeticOperation(a,b,symbol);
@@ -41,11 +39,6 @@ public class Calculator {
     }
 
     private static String[] validateInput(String input) throws Exception {
-        //Проверяем, что input не пустой, не NULL и не имеет букв
-        if (input.equals(null) || input.isEmpty() || !input.matches(".*[a-zA-Z\\\\u0400-\\\\u04FF].*")) {
-            throw new Exception("Невалидный ввод символа");
-        }
-
         // Убираем лишние пробелы и разделяем строку по пробелам
         String[] parts = input.trim().split(" ");
 
@@ -67,12 +60,6 @@ public class Calculator {
     private static void validateDiapason(int a, int b) throws Exception {
         if (a < 1 || a > 10 || b < 1 || b > 10) {
             throw new Exception("Числа должны быть в диапазоне 1-10");
-        }
-    }
-
-    private static void validateOperationSymbol(String symbol) throws Exception {
-        if (!symbol.equals("+") && !symbol.equals("-") && !symbol.equals("*") && !symbol.equals("/")) {
-            throw new Exception("Введен некорректный символ арифметической операции");
         }
     }
 
